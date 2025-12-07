b45d4e9379fba975414bd5edeb9077f0c81de09b
Ralf Joling <ralfjoling@gmail.com>
2025-12-07 20:52:16 +0100

    chore: Update version to v0.5.8

--- smart_heating/frontend/src/components/Header.tsx
+++ smart_heating/frontend/src/components/Header.tsx
@@ -112,7 +112,7 @@ const Header = ({ wsConnected = false }: HeaderProps) => {
             </Box>
           </Tooltip>
           <Chip 
-            label="v0.4.3" 
+            label="v0.5.8" 
             size="small" 
             variant="outlined"
             sx={{ 
